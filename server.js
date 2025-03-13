d4fbdf76d532e6cf2f140de41940227e7a95aedd
zain mp <wwwzainzaan@gmail.com>
2025-03-13 09:22:30 +0530

    fix cors

--- server.js
+++ server.js
@@ -10,12 +10,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 
-//  app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+ app.use(cors({ origin: "https://gl-mt-server-6agu.vercel.app", credentials: true }));
 
 // Routes
 app.use("/api/auth", authRoutes);
